d5d9f96d9b2d242742c3020a1717ba220fde90b0
YuraAlaverdyan <yuraalaverdyan@gmail.com>
2024-02-21 12:22:39 +0400

    Initial commit

---
+++ src/utils/router.tsx
@@ -0,0 +1,13 @@
+import {
+    Route,
+    createBrowserRouter,
+    createRoutesFromElements,
+} from "react-router-dom";
+import {Home} from "pages";
+
+
+export const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<Home/>}/>
+    )
+)
